4c6d2a9a5a5ce699ff750c9ac11fa110e04ff8bd
JohnRamos14 <Ramos.Johnmba@gmail.com>
2022-12-16 15:29:01 -0800

    added the home component to conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Youtube from  './components/video/Youtube';
-import Tasks from './components/Tasks/tasks';
-import MainNav from  './components/navbar/MainNav';
-import Multivideos from './components/video/Multivideos';
-import Home from './components/Home';
+import Youtube from "./components/video/Youtube";
+import Tasks from "./components/Tasks/tasks";
+import MainNav from "./components/navbar/MainNav";
+import Multivideos from "./components/video/Multivideos";
+import Home from "./components/Home";
 // import './App.css'
 
 function App() {
   return (
     <>
-    <div className='main'>
-    <MainNav/>
-    <Youtube />
-    <Home/>
+      <div className="main">
+        <MainNav />
 
-    </div>
-
-    <Routes>
-        <Route path="/Multivideos" element={<Multivideos />} />
-        <Route path="/tasks" element={<Tasks />} /> 
-    </Routes>
-
+        {/* Only render the Youtube component on the home page */}
+        {window.location.pathname === "/" && (
+          <>
+            <Home />
+            <Youtube />
+          </>
+        )}
+      </div>
+      <Routes>
+        <Route path="/multivideos" element={<Multivideos />} />
+        <Route path="/tasks" element={<Tasks />} />
+      </Routes>
     </>
-  ) 
+  );
 }
 
 export default App;
