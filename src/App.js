1ac3f875218ad604968239e8d23a188f96b419f2
JohnRamos14 <Ramos.Johnmba@gmail.com>
2022-12-16 14:50:25 -0800

    fixed video rendering on all routes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import React from 'react';
+import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Youtube from  './components/video/Youtube';
-import Tasks from './components/Tasks/tasks';
-import MainNav from  './components/navbar/MainNav';
-import Multivideos from './components/video/Multivideos';
+import Youtube from "./components/video/Youtube";
+import Tasks from "./components/Tasks/tasks";
+import MainNav from "./components/navbar/MainNav";
+import Multivideos from "./components/video/Multivideos";
 // import './App.css'
 
-
 function App() {
-  return(
+  return (
     <>
-      <MainNav/>
-    <Youtube />
-    <div className='main'>
-  
-     </div>
+    <div className="main">
+        <MainNav />
+        {/* Only render the Youtube component on the home page */}
+        {window.location.pathname === '/' && <Youtube />}
+      </div>
       <Routes>
         <Route path="/multivideos" element={<Multivideos />} />
-        <Route path="/tasks" element={<Tasks />} /> 
-    </Routes>  
-    
-   
-    </>
- 
-  ) 
+        <Route path="/tasks" element={<Tasks />} />
+      </Routes>
+  </>
+  );
 }
 
 export default App;
